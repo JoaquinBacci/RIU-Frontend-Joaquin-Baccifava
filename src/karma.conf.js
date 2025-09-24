dfb7d1ce0ee87896716c1fb118a089c263812e1c
JoaquinBacci <baccifavajoaquin@gmail.com>
2025-09-23 22:56:24 -0300

    chore: Archivos de configuración de tests

---
+++ src/karma.conf.js
@@ -0,0 +1,19 @@
+module.exports = function (config) {
+  config.set({
+    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage'),
+      require('@angular-devkit/build-angular/plugins/karma'),
+    ],
+    client: {
+      clearContext: false,
+    },
+    reporters: ['progress', 'kjhtml'],
+    browsers: ['ChromeHeadless'],
+    singleRun: false,
+    restartOnFileChange: true,
+  });
+};
